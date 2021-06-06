0c0fd1947ae22464ed20dbefbaf9001d14b7b059
NMHIEU2021999 <minhhieu20219992@gmail.com>
2021-06-06 11:35:39 +0700

    demo

--- Demo/src/App.js
+++ Demo/src/App.js
@@ -1,100 +1,112 @@
 import React from 'react'
-import data from './data/data'
 import { Form, Col, Row, Button } from 'react-bootstrap'
 import api from './api/api'
 import 'bootstrap/dist/css/bootstrap.min.css';
-import ApexChart from './Chart';
 class App extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
-      store: 1,
-      dept: 1,
-      data: []
+      text: '',
+      model: 'bilstm'
     }
   }
-
-  getStoreOption = () => {
-    let options = []
-    for (let key in data) {
-      options.push(
-        <option key={key} value={key}>{key}</option>
-      )
-    }
-    return options
+  changHandler = (e) => {
+    let name = e.target.name;
+    let value = e.target.value;
+    this.setState({ [name]: value })
   }
-
-  getDeptOption = (store) => {
-    let options = []
-    for (let key in data[store]) {
-      options.push(
-        <option key={key} value={key}>{key}</option>
-      )
-    }
-    return options
-  }
-
-  changeStore = (e) => {
-    this.setState({ store: e.target.value })
-  }
-  changeDept = (e) => {
-    this.setState({ dept: e.target.value })
-  }
-
   submit = async (e) => {
     e.preventDefault()
     try {
-      let res = await api.getPredict(this.state.store, this.state.dept)
-      let date = res.data.Date;
-      let weekly_sales = res.data.Weekly_Sales;
-      let len = date.length;
-      let data = [];
-      for (let i = 0; i < len; i++) {
-        let datum = {
-          x: new Date(date[i]).getTime(),
-          y: weekly_sales[i]
-        }
-        data.push(datum);
-      }
+      let res = await api.getPredict(this.state.text, this.state.model)
+      let data = res.data;
       this.setState({ data });
+      console.log(data)
     } catch (err) {
       console.log(err)
     }
   }
 
   render() {
+    let predictLabel = '';
+    let attention = '';
+    if (this.state.data) {
+      if (this.state.model === 'attention' || this.state.model === 'character-attention') {
+        attention = <tr>
+          <td className='min'><b>Attention score</b></td>
+          <td className='max'>{(this.state.data.sentiment_confidence * 100).toFixed(2)}%</td>
+          <td className='max'>{(this.state.data.topic_confidence * 100).toFixed(2)}%</td>
+        </tr>
+      }
+      predictLabel = <div>
+        <br></br>
+        <div className="row">
+          <table className='table table-bordered table-striped' style={{ width: '100%', fontSize: '22px' }}>
+            <thead>
+              <tr>
+                <th></th>
+                <th className='text-center'><h2>Sentiment </h2></th>
+                <th className='text-center'><h2>Topic</h2></th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td className='min'><b>Phân loại</b></td>
+                <td className='max'>{this.state.data.sentiment}</td>
+                <td className='max'>{this.state.data.topic}</td>
+              </tr>
+              <tr>
+                <td className='min'><b>Độ chắc chắn</b></td>
+                <td className='max'>{(this.state.data.sentiment_confidence * 100).toFixed(2)}%</td>
+                <td className='max'>{(this.state.data.topic_confidence * 100).toFixed(2)}%</td>
+              </tr>
+              {attention}
+            </tbody>
+          </table>
+        </div>
+      </div>
+
+    }
     return (
       <div className="container">
-        <Form>
+        <Form onSubmit={this.submit}>
           <Form.Group controlId="exampleForm.ControlSelect1">
+            <br />
             <Row>
-              <div className="h1">Demo</div>
+              <div className="h1">Demo nhóm 19 - Phân tích quan điểm</div>
             </Row>
+            <br />
+            <br />
+            <br />
             <Row>
-              <Col>
-                <Form.Label>Store</Form.Label>
-                <Form.Control as="select" onChange={this.changeStore}>
-                  {this.getStoreOption()}
+              <Col xs={8}>
+                <Form.Label><h6>Câu thử nghiệm</h6></Form.Label>
+                <Form.Control as="textarea" value={this.state.text} required name="text" rows={4} onChange={this.changHandler} placeholder="Nhập câu cần dự đoán">
                 </Form.Control>
               </Col>
-              <Col>
-                <Form.Label>Department</Form.Label>
-                <Form.Control as="select" onChange={this.changeDept}>
-                  {this.getDeptOption(this.state.store)}
+              <Col xs={4}>
+                <Form.Label><h6>Model</h6></Form.Label>
+                <Form.Control as="select" value={this.state.model} name="model" onChange={this.changHandler}>
+                  <option value="bilstm">Bi-LSTM</option>
+                  <option value="character">Character</option>
+                  <option value="attention">Attention</option>
+                  <option value="character-attention">Character attention</option>
                 </Form.Control>
               </Col>
             </Row>
           </Form.Group>
+          <br />
+          <br />
           <Form.Group>
-            <Row>
-              <Col>
-                <Button variant="primary" type="submit" className="w-100" onClick={this.submit}>Submit</Button>
+            <Form.Row className="justify-content-center">
+              <Col xs={4}>
+                <Button variant="primary" type="submit" className="w-100">Dự đoán</Button>
               </Col>
-            </Row>
+            </Form.Row>
           </Form.Group>
         </Form>
         <div>
-          <ApexChart data={this.state.data} />
+          {predictLabel}
         </div>
       </div>
     )
